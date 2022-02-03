2f0d169e1afc7a587537209fb07b6d90f6733b25
Kenneth Chen <kenchen0606@gmail.com>
2022-02-03 16:42:29 -0500

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 
 import Header from './Header.jsx';
 
 const App = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://aircall-job.herokuapp.com/activities`)
+      .then((res) => {
+        setActivities([...res.data])
+      })
+  }, []);
+
   return (
     <div className='container'>
       <Header/>
