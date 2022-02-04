206b58361a8679056bc48d633580c5ffe3227b8f
Kenneth Chen <kenchen0606@gmail.com>
2022-02-03 22:56:01 -0500

    add bottom navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
+import { AppBar, Toolbar, Box, Fab, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+// import  from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MenuIcon from '@mui/icons-material/Menu';
 
+import NavBar from './components/NavBar.jsx';
 import Header from './Header.jsx';
 
 const App = () => {
@@ -14,10 +23,46 @@ const App = () => {
       })
   }, []);
 
+  const StyledFab = styled(Fab)({
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  });
+
   return (
     <div className='container'>
       <Header/>
-      <div className="container-view">Some activities should be here</div>
+      <div className="container-view">
+        Some activities should be here
+
+        
+      
+      </div>
+
+      <AppBar position="relative" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+          <IconButton color="inherit" onClick={() => { console.log('LocalPhoneIcon'); }}>
+            <LocalPhoneIcon />
+          </IconButton>
+          <IconButton color="inherit" onClick={() => { console.log('PersonOutlineIcon'); }}>
+            <PersonOutlineIcon />
+          </IconButton>
+          <StyledFab color="secondary" aria-label="add">
+            <AddIcon />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+         
+          <IconButton color="inherit" onClick={() => { console.log('SettingsIcon'); }}>
+            <SettingsIcon />
+          </IconButton>
+          <IconButton color="inherit" onClick={() => { console.log('MenuIcon'); }}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
